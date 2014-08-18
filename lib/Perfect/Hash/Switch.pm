72f3f6f6dc73a7c51f73b5693df5b5988518600b
Reini Urban <rurban@cpanel.net>
2014-08-18 09:44:48 -0500

    Switch: fixed handling of last buckets, optimized to use faster word cmp

--- lib/Perfect/Hash/Switch.pm
+++ lib/Perfect/Hash/Switch.pm
@@ -5,6 +5,7 @@ our $VERSION = '0.01';
 #use warnings;
 use Perfect::Hash;
 use Perfect::Hash::C;
+use Config;
 use integer;
 use bytes;
 our @ISA = qw(Perfect::Hash Perfect::Hash::C);
@@ -88,22 +89,61 @@ sub save_c {
   my ($old, @cand);
   for my $s (sort { length($a) <=> length($b) } keys %$dict) {
     my $l = bytes::length($s);
+    #print "l=$l, old=$old, s=$s\n" if $options->{-debug};
     $old = $l unless defined $old;
-    if ($l == $old) {
-      push @cand, $s;
-    } elsif (@cand) {
-      do_cand($FH, $old, \@cand, $dict);
+    if ($l != $old and @cand) {
+      print "dump l=$old: [",join(" ",@cand),"]\n" if $options->{-debug};
+      _do_cand($FH, $old, \@cand, $dict);
       @cand = ();
-      $old = $l;
     }
+    push @cand, $s;
+    #print "push $s\n" if $options->{-debug};
+    $old = $l;
   }
-  do_cand($FH, $old, \@cand, $dict) if @cand;
+  print "rest l=$old: [",join(" ",@cand),"]\n" if $options->{-debug};
+  _do_cand($FH, $old, \@cand, $dict) if @cand;
   print $FH "
     }
 }
 ";
 }
-sub do_cand {
+
+# length optimized memcmp
+# it's the last statement if $last, otherwise as fallthrough to the next case statement.
+# TODO: do away with most memcmp for shorter strings (< 128?)
+sub _cmp {
+  my ($s, $l, $v, $last) = @_;
+  my $cmp;
+  if ($l == 1) {
+    my $ord = ord($s);
+    if ($ord >= 40 and $ord < 127) {
+      $cmp = "*s == '$s'";
+    } else {
+      $cmp = "*s == $ord /* $s */";
+    }
+  } elsif ($l == 2) {
+    my $short = sprintf("0x%x", unpack("S", $s));
+    $cmp = "*(short*)s == $short /* $s */";
+  } elsif ($l == 4) {
+    my $int = sprintf("0x%x", unpack("L", $s));
+    $cmp = "*(int*)s == $int /* $s */";
+  } elsif ($Config{d_quad} and $l == 8) {
+    my $quad = sprintf("0x%lx", unpack("Q", $s));
+    my $quadtype = $Config{uquadtype};
+    $cmp = "*($quadtype *)s == $quad /* $s */";
+  } else {
+    $cmp = "!memcmp(s, ".B::cstring($s).", $l)";
+  }
+  if ($last) {
+    return "
+        return $cmp ? $v : -1;";
+  } else {
+    return "
+        if ($cmp) return $v;";
+  }
+}
+
+sub _do_cand {
   my ($FH, $l, $cand, $dict) = @_;
   # switch on length
   print $FH "
@@ -111,22 +151,14 @@ sub do_cand {
   if (@$cand == 1) { # only one candidate to check
     my $s0 = $cand->[0];
     my $v = $dict->{$s0};
-    if ($l == 1) {
-      $s0 = substr(B::cstring($s0),1,-1);
-      $s0 =~ s/'/\\'/;
-      print $FH "
-      return *s == '$s0' ? $v : -1;";
-    } else {
-      print $FH "
-      return memcmp(s, ",B::cstring($cand->[0]),", $l) ? -1 : $v;";
-    }
+    print $FH _cmp($s0, $l, $v, 1);
   } else {
     # switch on the most diverse char in the strings
-    do_switch($FH, $cand, $dict);
+    _do_switch($FH, $cand, $dict);
   }
 }
 
-sub do_switch {
+sub _do_switch {
   my ($FH, $cand, $dict) = @_;
   # find the best char in @cand to switch on
   my $maxkeys = [0,0,undef];
@@ -157,29 +189,22 @@ sub do_switch {
     # TODO: if $h{$c} > 5-10 nest one more switch recursively
     if (0 and $h->{$c} > 8) {
       my @cand_c = map { substr($_,$i,1) eq $c } @$cand;
-      do_switch($FH, \@cand_c, $dict);
+      _do_switch($FH, \@cand_c, $dict);
     } else {
       my $v = $dict->{$s};
-      my $qc = substr(B::cstring($c),1,-1);
-      $qc =~ s/'/\\'/g;
+      my $ord = ord($c);
+      my $case = ($ord >= 40 and $ord < 127) ? "'$c':" : "$ord: /* $c */";
       if ($h->{$c} == 1) {
         print $FH "
-      case '$qc':";
-        if (length($s) == 1) {
-          print $FH "
-        return s[$i] == '$qc' ? $v : -1;";
-        } else {
-          print $FH "
-        return memcmp(s, ",B::cstring($s),", $l) ? -1 : $v;";
-        }
+      case $case";
+        print $FH _cmp($s, $l, $v, 1);
       } else {
         if ($c ne $old_c) {
           print $FH "
-      case '$qc':";
+      case $case";
           $new_case = 1;
         }
-        print $FH "
-        if (!memcmp(s, ",B::cstring($s),", $l)) return $v;";
+        print $FH _cmp($s, $l, $v);
         $old_c = $c;
       }
     }
@@ -203,6 +228,8 @@ sub perfecthash {
 }
 
 =item false_positives
+
+Returns undef, always checks the keys.
 
 =cut
 
@@ -233,7 +260,7 @@ sub c_lib { "" }
 =cut
 
 unless (caller) {
-  __PACKAGE__->new("examples/words500")->save_c;
+  __PACKAGE__->new("examples/words20",@ARGV)->save_c;
 }
 
 1;

4da71ce3c86ec5ae81129703a86a539a057459f8
Reini Urban <rurban@cpanel.net>
2014-08-22 23:37:54 -0500

    add examples/all-bench.sh

---
+++ examples/all-bench.sh
@@ -0,0 +1,14 @@
+#/bin/sh
+p=perl5.20.0-nt
+
+make clean
+$p Makefile.PL && make -s
+cd bob; git checkout Makefile; rm Makefile.bak; cd ..
+g=`git describe --long --tags --dirty --always`
+
+$p -Mblib examples/bench.pl                        | tee log.bench-$g
+$p -Mblib examples/bench.pl -size 25000 -nul -1opt | tee log.bench-$g-25000
+$p -Mblib examples/bench.pl -size 10000 -nul -1opt | tee log.bench-$g-10000
+$p -Mblib examples/bench.pl -size 2000 -nul -1opt  | tee log.bench-$g-2000
+$p -Mblib examples/bench.pl -size 500 -nul         | tee log.bench-$g-500
+$p -Mblib examples/bench.pl -siez 127 -nul         | tee log.bench-$g-127
